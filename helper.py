e6176af6d57550faf4fbd6cece37f57117f75aa4
san4o101 <snikersmc123@gmail.com>
2018-11-24 22:04:57 +0200

    + setup ORM + BaseModel

---
+++ helper.py
@@ -0,0 +1,27 @@
+import os
+import sqlalchemy as sa
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy_mixins import ReprMixin, SmartQueryMixin, ActiveRecordMixin
+
+Base = declarative_base()
+
+
+class BaseModel(Base, SmartQueryMixin, ActiveRecordMixin, ReprMixin):
+    __abstract__ = True
+    __repr__ = ReprMixin.__repr__
+    pass
+
+#################### setup ORM ######################
+
+
+db_file = os.path.join(os.path.dirname(__file__), 'test.sqlite')
+engine = sa.create_engine('mysql://root:123@localhost:3306/m', echo=True) # False to force
+session = scoped_session(sessionmaker(bind=engine))
+
+BaseModel.set_session(session)
+
+
+def log(msg):
+    print('\n{}\n'.format(msg))
