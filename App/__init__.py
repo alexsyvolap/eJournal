835fb7dc2a519df7bc35a6422bf87d0a7985d8a3
san4o101 <snikersmc123@gmail.com>
2018-12-01 17:13:32 +0200

    ~ add log setup

--- App/__init__.py
+++ App/__init__.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 __version__ = '0.1'
 from flask import Flask
+from logging.handlers import RotatingFileHandler
+import datetime
+import logging
+from settings import HOST
 
-app = Flask('project')
+app = Flask('App')
 app.config['JSON_AS_ASCII'] = False
-app.config['SECRET_KEY'] = 'random'
-app.debug = True
+app.config['SECRET_KEY'] = HOST['secret']
+app.debug = False
+
+#################### setup LOG ######################
+
+
+handler = RotatingFileHandler('/var/log/eJournal/log-{0}.log'.format(datetime.date.today()), maxBytes=10000, backupCount=1)
+app.logger.addHandler(handler)
+logging.getLogger('werkzeug').setLevel(logging.DEBUG)
+logging.getLogger('werkzeug').addHandler(handler)
+app.logger.setLevel(logging.WARNING)
+app.logger.setLevel(logging.INFO)
+app.logger.addHandler(handler)
+
 
 import App.Controllers.LoginController
