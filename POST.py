07b084598d69b4f36e6ea6f34e819a5f2fbb73d3
san4o101 <snikersmc123@gmail.com>
2018-12-02 17:50:37 +0200

    ~ add route /api/group/subjects

--- POST.py
+++ POST.py
@@ -4,12 +4,18 @@ from settings import URL
 url = URL['test']
 
 login = URL['login']
-dataLogin = URL['jsonStudent']
+dataLogin = URL['jsonTeacher']
 r = requests.post(url=url+login, json=dataLogin)
 print(r.url, r.json())
 token = r.json()['token']
 
 login = URL['group']
 dataLogin = {'token': token}
+r = requests.post(url=url+login, json=dataLogin)
+print(r.url, r.json())
+
+group_id = 1
+login = URL['subjects']
+dataLogin = {'token': token, 'group_id': group_id}
 r = requests.post(url=url+login, json=dataLogin)
 print(r.url, r.json())
